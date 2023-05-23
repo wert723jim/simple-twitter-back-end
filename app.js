4144bf8cb0db103ee0bfa9b6bedee02a127f17a6
LAPTOP-DHHLQT9N\wert7 <wert723jim@gmail.com>
2023-05-23 22:50:01 +0800

    feat: signup and hash password and column check

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const helpers = require('./_helpers');
 const app = express();
+// 測試 models 內的檔案是否有正常運作，有問題會報錯
+const db = require('./models')
+const port = 3000
 
 // use helpers.getUser(req) to replace req.user
 function authenticated(req, res, next) {
   // passport.authenticate('jwt', { ses...
 }
 // app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 // route
 app.use('/api', require('./routes/api'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
 module.exports = app;
