fe5a49d0ab290a174ac10e374997bffb5b710c67
Allen_Yeh <happy1294123@gmail.com>
2023-05-23 20:13:43 +0800

    test:git flow

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const helpers = require('./_helpers');
 const app = express();
+const passport = require('./config/passport');
+
+const User = require('./models')['User'];
+User.create({
+  account: 'allen',
+  name: 'allen',
+});
 
 // use helpers.getUser(req) to replace req.user
 function authenticated(req, res, next) {
   // passport.authenticate('jwt', { ses...
 }
-// app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
+
+app.use(passport.initialize());
+
+const path = require('path');
+app.get('/test_login', (req, res) =>
+  res.sendFile(path.join(__dirname, 'test_login.html'))
+);
+app.post(
+  '/test_login',
+  passport.authenticate('local', {
+    session: false,
+    failureRedirect: '/login_fail',
+  }),
+  (req, res) => {
+    console.log('login success');
+    res.send('login success');
+  }
+);
+
+app.get('/login_fail', (req, res) => res.send('login fail'));
 
 // route
 app.use('/api', require('./routes/api'));
