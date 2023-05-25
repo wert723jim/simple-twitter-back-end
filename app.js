ee6268f4bb04f3350ea8eb9e99bf43721281dbb2
Allen_Yeh <happy1294123@gmail.com>
2023-05-25 15:35:44 +0800

    refactor:整理整理app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const helpers = require('./_helpers')
 const app = express()
-
 const passport = require('./config/passport')
 
 // use helpers.getUser(req) to replace req.user
@@ -12,24 +11,6 @@ function authenticated(req, res, next) {
 app.use(express.urlencoded({ extended: true }))
 
 app.use(passport.initialize())
-
-// const path = require('path')
-// app.get('/test_login', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'test_login.html'))
-// )
-// app.post(
-//   '/test_login',
-//   passport.authenticate('local', {
-//     session: false,
-//     failureRedirect: '/login_fail',
-//   }),
-//   (req, res) => {
-//     console.log('login success')
-//     res.send('login success')
-//   }
-// )
-
-// app.get('/login_fail', (req, res) => res.send('login fail'))
 
 // route
 app.use('/api', require('./routes/api'))
--- config/passport.js
+++ config/passport.js
@@ -5,6 +5,8 @@ passport.use(
   new LocalStrategy((username, password, done) => {
     console.log(username)
     console.log(password)
+    // return done(null, false, { message: 'this is not OK' })
+    return done('errrrrror', false, { message: 'this is not OK' })
     const user = { username, password }
     return done(null, user)
   })
